cf552c10c9443d1a73e88b3b455b156eb96531bd
Raveendrababu Battula <battula32@hotmail.com>
2024-11-11 15:07:16 -0500

    Singleton sample

---
+++ SingletonSample/TestSingle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonSample
+{
+    internal class TestSingle
+    {
+        private static TestSingle testSingleObject = new TestSingle();
+        private TestSingle()
+        {
+            Console.WriteLine("Constructor called");
+        }
+
+        public static TestSingle TestSingleObject
+        {
+            get
+            {
+                return testSingleObject;
+            }
+        }
+
+        public void DoSomeThing(int i)
+        {
+            Console.WriteLine($"Do some thing {i}");
+        }
+    }
+}
---
+++ StringReverse/TestSingle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverse
+{
+    internal class TestSingle
+    {
+        private static TestSingle testSingleObject = new TestSingle();
+        private TestSingle()
+        {
+            Console.WriteLine("Constructor called");
+        }
+
+        public static TestSingle TestSingleObject
+        {
+            get
+            {
+                return testSingleObject;
+            }
+        }
+    }
+}
