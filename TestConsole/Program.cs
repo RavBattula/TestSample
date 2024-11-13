2bcf53a92f4d1a7fd1260b9444cc7f27ba1b875f
Raveendrababu Battula <battula32@hotmail.com>
2024-11-13 12:06:52 -0500

    Few more test samples added (Inheritance and operator overloading)

---
+++ TestConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int[] myArr = { 1, 2, 3 };
+        foreach (int i in myArr)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("Changing the size of the array");
+        Array.Resize<int>(ref myArr, 10);
+        foreach (int i in myArr)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.Read();
+    }
+}
